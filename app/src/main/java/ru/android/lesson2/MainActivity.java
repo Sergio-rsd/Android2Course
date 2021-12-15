620714272c9ee279288718f010966d171b4daebc
Sergio-rsd <sergsa2001@mail.ru>
2021-12-15 21:31:38 +0300

    Основные кнопки описаны

--- app/src/main/java/ru/android/lesson2/MainActivity.java
+++ app/src/main/java/ru/android/lesson2/MainActivity.java
@@ -34,10 +34,12 @@ public class MainActivity extends AppCompatActivity {
     private void initView() {
         expression = findViewById(R.id.place_result);
         initOperationsButton();
-        initButton7Click();
+        initNumberButton();
+//        initButton7Click();
     }
 
     // button of operation
+
     private void initOperationsButton() {
         Button buttonMinus = findViewById(R.id.button_minus);
         buttonMinus.setOnClickListener(buttonMinusClickListener);
@@ -51,6 +53,12 @@ public class MainActivity extends AppCompatActivity {
         Button buttonDivide = findViewById(R.id.button_divide);
         buttonDivide.setOnClickListener(buttonDivideClickListener);
 
+        Button buttonComma = findViewById(R.id.button_comma);
+        buttonComma.setOnClickListener(buttonCommaClickListener);
+
+        Button buttonPercent = findViewById(R.id.button_percent);
+        buttonPercent.setOnClickListener(buttonPercentClickListener);
+
     }
 
     public View.OnClickListener buttonMinusClickListener = new View.OnClickListener() {
@@ -59,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
             setExpressionInTextView(expression, operationsButton.getMINUS());
         }
     };
+
     public View.OnClickListener buttonPlusClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -77,26 +86,120 @@ public class MainActivity extends AppCompatActivity {
             setExpressionInTextView(expression, operationsButton.getDIVIDE());
         }
     };
-
+    public View.OnClickListener buttonCommaClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            setExpressionInTextView(expression, operationsButton.getCOMMA());
+        }
+    };
+    public View.OnClickListener buttonPercentClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            setExpressionInTextView(expression, operationsButton.getPERCENT());
+        }
+    };
     // button of numbers
-    private void initButton7Click() {
+
+    private void initNumberButton() {
+        Button button0 = findViewById(R.id.button_0);
+        button0.setOnClickListener(button0ClickListener);
+
+        Button button1 = findViewById(R.id.button_1);
+        button1.setOnClickListener(button1ClickListener);
+
+        Button button2 = findViewById(R.id.button_2);
+        button2.setOnClickListener(button2ClickListener);
+
+        Button button3 = findViewById(R.id.button_3);
+        button3.setOnClickListener(button3ClickListener);
+
+        Button button4 = findViewById(R.id.button_4);
+        button4.setOnClickListener(button4ClickListener);
+
+        Button button5 = findViewById(R.id.button_5);
+        button5.setOnClickListener(button5ClickListener);
+
+        Button button6 = findViewById(R.id.button_6);
+        button6.setOnClickListener(button6ClickListener);
+
         Button button7 = findViewById(R.id.button_7);
         button7.setOnClickListener(button7ClickListener);
+
+        Button button8 = findViewById(R.id.button_8);
+        button8.setOnClickListener(button8ClickListener);
+
+        Button button9 = findViewById(R.id.button_9);
+        button9.setOnClickListener(button9ClickListener);
     }
 
+    public View.OnClickListener button0ClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            setExpressionInTextView(expression, numbersButton.getNUMBER_0());
+        }
+    };
+    public View.OnClickListener button1ClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            setExpressionInTextView(expression, numbersButton.getNUMBER_1());
+        }
+    };
+    public View.OnClickListener button2ClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            setExpressionInTextView(expression, numbersButton.getNUMBER_2());
+        }
+    };
+    public View.OnClickListener button3ClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            setExpressionInTextView(expression, numbersButton.getNUMBER_3());
+        }
+    };
+    public View.OnClickListener button4ClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            setExpressionInTextView(expression, numbersButton.getNUMBER_4());
+        }
+    };
+    public View.OnClickListener button5ClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            setExpressionInTextView(expression, numbersButton.getNUMBER_5());
+        }
+    };
+    public View.OnClickListener button6ClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            setExpressionInTextView(expression, numbersButton.getNUMBER_6());
+        }
+    };
     public View.OnClickListener button7ClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             setExpressionInTextView(expression, numbersButton.getNUMBER_7());
         }
     };
+    public View.OnClickListener button8ClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            setExpressionInTextView(expression, numbersButton.getNUMBER_8());
+        }
+    };
+    public View.OnClickListener button9ClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            setExpressionInTextView(expression, numbersButton.getNUMBER_9());
+        }
+    };
 
     private void setExpressionInTextView(TextView expression, int number) {
-        expression.setText(expression.getText() + String.format(Locale.getDefault(), "%d", number));
+        expression.setText(new StringBuilder().append(expression.getText()).append(String.format(Locale.getDefault(), "%d", number)).toString());
     }
 
     private void setExpressionInTextView(TextView expression, char number) {
-        expression.setText(expression.getText() + String.format(Locale.getDefault(), "%c", number));
+//        expression.setText(expression.getText() + String.format(Locale.getDefault(), "%c", number));
+        expression.setText(new StringBuilder().append(expression.getText()).append(String.format(Locale.getDefault(), "%c", number)).toString());
     }
 
 }
--- app/src/main/java/ru/android/lesson2/OperationsButton.java
+++ app/src/main/java/ru/android/lesson2/OperationsButton.java
@@ -1,20 +1,19 @@
 package ru.android.lesson2;
 
 public class OperationsButton {
-    private final char DIVIDE ='/';
-    private final char MULTIPLY ='*';
-    private final char MINUS ='-';
-    private final char PLUS ='+';
-    private final char PERCENT ='%';
-    private final char COMMA ='.';
-
-    public char getCOMMA() {
-        return COMMA;
-    }
+    private final char DIVIDE = '/';
+    private final char MULTIPLY = '*';
+    private final char MINUS = '-';
+    private final char PLUS = '+';
+    private final char PERCENT = '%';
+    private final char COMMA = '.';
 
     public OperationsButton() {
     }
 
+    public char getCOMMA() {
+        return COMMA;
+    }
 
     public char getDIVIDE() {
         return DIVIDE;
